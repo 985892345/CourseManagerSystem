d99a44bd5793fbf35e60cce8b0c26abea536b8f2
985892345 <guo985892345@foxmail.com>
2024-03-23 12:37:38 +0800

    :zap: dialog 隐藏时卡顿优化

--- course-app/components/base/src/commonMain/kotlin/com/course/components/base/ui/dialog/DialogCompose.kt
+++ course-app/components/base/src/commonMain/kotlin/com/course/components/base/ui/dialog/DialogCompose.kt
@@ -1,17 +1,38 @@
 package com.course.components.base.ui.dialog
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animate
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.StateFactoryMarker
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.course.components.utils.compose.derivedStateOfStructure
+import com.course.components.utils.coroutine.AppCoroutineScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 /**
  * .
@@ -50,7 +71,15 @@ abstract class Dialog {
   fun show() {
     if (AppDialogState === Empty) {
       AppDialogState = this
-      AppDialogVisible = true
+      if (AppDialogCoroutineScope == null) {
+        AppDialogAlpha.tryEmit(1F)
+        return
+      }
+      AppDialogCoroutineScope?.launch {
+        animate(initialValue = AppDialogAlpha.value, targetValue = 1F) { value, _ ->
+          AppDialogAlpha.tryEmit(value)
+        }
+      }
     } else {
       if (AppDialogState === this) return
       var l = 0
@@ -70,7 +99,20 @@ abstract class Dialog {
 
   fun hide() {
     if (AppDialogState === this) {
-      AppDialogVisible = false
+      if (AppDialogAlphaAnimHideJob != null) return // 说明正在隐藏
+      // 此时要么正在执行显示的动画，要么显示中
+      if (AppDialogList.isNotEmpty()) {
+        AppDialogState = AppDialogList.removeLastOrNull()!!
+      } else {
+        AppDialogAlphaAnimHideJob = AppDialogCoroutineScope?.launch {
+          animate(initialValue = AppDialogAlpha.value, targetValue = 0F) { value, _ ->
+            AppDialogAlpha.tryEmit(value)
+          }
+          AppDialogAlphaAnimHideJob = null
+          AppDialogState = Empty
+          AppDialogList.removeLastOrNull()?.show()
+        }
+      }
       return
     }
     AppDialogList.remove(this)
@@ -89,29 +131,36 @@ abstract class Dialog {
 
 private val AppDialogList = mutableListOf<Dialog>()
 
-private var AppDialogVisible by mutableStateOf(false)
+private var AppDialogAlpha = MutableStateFlow(0F)
+
+private var AppDialogAlphaAnimHideJob: Job? = null
 
 private var AppDialogState: Dialog by mutableStateOf(Dialog.Empty)
 
+private var AppDialogCoroutineScope: CoroutineScope? = null
+
 @Composable
 internal fun DialogCompose() {
-  AnimatedVisibility(visible = AppDialogVisible, enter = fadeIn(), exit = fadeOut()) {
-    Dialog(
-      properties = AppDialogState.properties,
-      onDismissRequest = {
-        AppDialogState.onDismissRequest.invoke(AppDialogState)
-      }
-    ) {
-      AppDialogState.Content()
-    }
-    DisposableEffect(Unit) {
-      onDispose {
-        val last = AppDialogList.removeLastOrNull()
-        if (last == null) {
-          AppDialogState = Dialog.Empty
-        } else {
-          AppDialogState = last
-          AppDialogVisible = true
+  AppDialogCoroutineScope = rememberCoroutineScope()
+  val dialogAlpha by AppDialogAlpha.collectAsState()
+  if (!remember { derivedStateOfStructure { dialogAlpha == 0F } }.value) {
+    Box(modifier = Modifier.fillMaxSize().graphicsLayer {
+      // 官方的 AnimatedVisibility 动画要慢一帧，应该是需要重组的原因，看起来感觉就像停顿了一下，
+      // 所以改为自定义 alpha 实现
+      alpha = dialogAlpha
+    }) {
+      Dialog(
+        properties = AppDialogState.properties,
+        onDismissRequest = {
+          AppDialogState.onDismissRequest.invoke(AppDialogState)
+        }
+      ) {
+        Card(
+          modifier = Modifier.wrapContentSize(),
+          shape = RoundedCornerShape(16.dp),
+          backgroundColor = Color.White,
+        ) {
+          AppDialogState.Content()
         }
       }
     }
